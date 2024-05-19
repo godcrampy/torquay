ddae38d85a5ae447be28be4df1fd468514732d91
Sahil Bondre <sahilbondre@gmail.com>
2024-05-19 14:35:09 +0100

    TRQ-001: Setup stateless singleton server (#2)

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("Welcome to Torquay")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	mode := os.Getenv("ENV")
+	gin.SetMode(mode)
+
+	r := gin.Default()
+
+	token := 1
+
+	r.GET("/api/v1/token", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{
+			"token": token,
+		})
+
+		token += 1
+	})
+
+	port := os.Getenv("PORT")
+	log.Printf("INFO: Starting server on port %s\n", port)
+	log.Printf("INFO: Server running in %s mode\n", mode)
+	if err := r.Run(); err != nil {
+		log.Fatalf("Failed to run server: %v", err)
+	}
 }
